8fb7d84150fcef652d08686d1d8533caa3988d67
Galina Dzhasova <casperbook@Galinas-MacBook-Pro.local>
2021-01-24 00:48:59 -0500

    solution

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -5,9 +5,24 @@
 
   function LunchCheckController($scope, $filter, $injector){
 
+      $scope.validateDishesCount = function(){
+        let dishInput = $scope.dishes;
 
-    console.log($injector.annotate(LunchCheckController));
+        if(dishInput){
+          let dishesActualCount = 0;
+          let dishInputArr = dishInput.split(',');
+          dishInputArr.forEach((item, i) => {
+            if(item.trim().length > 0){
+              dishesActualCount++;
+            }
+          });
+
+          $scope.validationMessage = dishesActualCount < 4 ? 'Enjoy!' : 'Too much!';
+        }
+        else {
+          $scope.validationMessage = "Please enter data first.";
+        }
+      };
   };
-
 
 })();
